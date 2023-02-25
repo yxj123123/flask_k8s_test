70e6d62f2a089ca0c50f1ee8c994601864433bb8
yxj123123 <115879350+yxj123123@users.noreply.github.com>
2023-02-25 17:50:11 +0800

    Delete database_experiment directory

--- database_experiment/config.py
+++
@@ -1,9 +0,0 @@
-SECRET_KEY = "asdfasdfjasdfjasd;lf"
-#数据库配置
-HOSTNAME = '127.0.0.1'
-PORT     = '3306'
-DATABASE = 'database_experiment'
-USERNAME = 'root'
-PASSWORD = 'root'
-DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
-SQLALCHEMY_DATABASE_URI = DB_URI
--- database_experiment/exts.py
+++
@@ -1,7 +0,0 @@
-# 这个文件存在的意义是为了解决循环引用问题
-# 1.flask db init：这步只需要执行一次
-# 2.flask db migrate：识别ORM模型的改变，生成迁移脚步
-# 3.flask db upgrade：运行迁移脚本
-
-from flask_sqlalchemy import SQLAlchemy
-db=SQLAlchemy()
